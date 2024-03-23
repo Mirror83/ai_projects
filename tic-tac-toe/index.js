a38481694a3e09b90c8f6750de0daa5c5edb2246
Mirror83 <glenochieng045@gmail.com>
2024-03-23 09:26:17 +0300

    Refactored to using a backing board  array instead of the NodeList containing the elements for the logic

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,7 +1,6 @@
-const cells = document.querySelectorAll(".cell");
-const turnIndicator = document.querySelector("#turn-indicator");
+const PLAYER_ONE_TURN = 0;
+const PLAYER_TWO_TURN = 1;
 
-let turn = 0;
 const PLAYER_ONE_TOKEN = "X";
 const PLAYER_TWO_TOKEN = "O";
 const EMPTY_CELL_TOKEN = "";
@@ -10,21 +9,47 @@ const WIN_STATE = "win";
 const DRAW_STATE = "draw";
 const PLAY_STATE = "play";
 
-function content(cell) {
-  const span = cell.querySelector("span");
-  if (!span) return EMPTY_CELL_TOKEN;
+function clearBoard() {
+  for (var i = 0; i < 9; i++) {
+    board.push(EMPTY_CELL_TOKEN);
+  }
+}
 
-  return span.textContent;
+function isAvailable(index) {
+  return board[index] === EMPTY_CELL_TOKEN;
+}
+
+function placeToken(index) {
+  if (!isAvailable(index)) return;
+
+  if (turn === PLAYER_ONE_TURN) {
+    board[index] = PLAYER_ONE_TOKEN;
+  } else {
+    board[index] = PLAYER_TWO_TOKEN;
+  }
+
+  placeTokenUi(index);
+}
+
+function placeTokenUi(index) {
+  const tokenSpan = document.createElement("span");
+  if (turn === PLAYER_ONE_TURN) {
+    tokenSpan.innerText = PLAYER_ONE_TOKEN;
+  } else {
+    tokenSpan.innerText = PLAYER_TWO_TOKEN;
+  }
+
+  cells[index].appendChild(tokenSpan);
 }
 
 function matches(indexOne, indexTwo, indexThree) {
   return (
-    content(cells[indexOne]) === content(cells[indexTwo]) &&
-    content(cells[indexOne]) === content(cells[indexThree]) &&
-    content(cells[indexTwo]) === content(cells[indexThree]) &&
-    content(cells[indexOne]) != EMPTY_CELL_TOKEN &&
-    content(cells[indexTwo]) != EMPTY_CELL_TOKEN &&
-    content(cells[indexThree]) != EMPTY_CELL_TOKEN
+    board[indexOne] === board[indexTwo] &&
+    board[indexOne] === board[indexThree] &&
+    board[indexTwo] === board[indexThree] &&
+    board[indexOne] != EMPTY_CELL_TOKEN &&
+    board[indexTwo] != EMPTY_CELL_TOKEN &&
+    board[indexThree] != EMPTY_CELL_TOKEN
   );
 }
 
@@ -60,6 +85,11 @@ function diagonalFromTwoMatches() {
   return matches(2, 4, 6);
 }
 
+function updateTurn() {
+  turn = (turn + 1) % 2;
+  turnIndicator.textContent = turn + 1;
+}
+
 function isWin() {
   return (
     firstRowMatches() ||
@@ -76,46 +106,35 @@ function isWin() {
 function isDraw() {
   // Assume that the current board state does not contain a win false
   // Just checks if the board is filled
-  for (const cell of cells) {
-    if (!cell.querySelector("span")) return false;
+  for (const token of board) {
+    if (token == EMPTY_CELL_TOKEN) return false;
   }
 
   return true;
 }
 
-function isAvailable(index) {
-  return cells[index].querySelector("span");
-}
-
 function checkBoard() {
-  if (isWin()) {
-    return WIN_STATE;
-  } else if (isDraw()) return DRAW_STATE;
+  if (isWin()) return WIN_STATE;
+  else if (isDraw()) return DRAW_STATE;
   else return PLAY_STATE;
 }
 
-cells.forEach((cell) => {
+const cells = document.querySelectorAll(".cell");
+const turnIndicator = document.querySelector("#turn-indicator");
+let turn = PLAYER_ONE_TURN;
+
+const board = [];
+
+clearBoard();
+
+cells.forEach((cell, index) => {
   cell.addEventListener("click", () => {
-    console.log(turn);
-
-    if (cell.querySelector("span")) {
-      return;
-    }
-
-    const tokenSpan = document.createElement("span");
-    if (turn == 0) {
-      tokenSpan.innerText = PLAYER_ONE_TOKEN;
-    } else {
-      tokenSpan.innerText = PLAYER_TWO_TOKEN;
-    }
-
-    cell.appendChild(tokenSpan);
+    placeToken(index);
 
     const boardState = checkBoard();
 
     if (boardState === PLAY_STATE) {
-      turn = (turn + 1) % 2;
-      turnIndicator.textContent = turn + 1;
+      updateTurn();
     } else if (boardState === WIN_STATE) {
       alert(`Player ${turn + 1} has won!`);
     } else {
